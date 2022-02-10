93120406e9ae58f96c53f9d85fc978a4555c4f8a
Toby Artisan <tobyartisan@outlook.com>
2022-02-09 20:23:31 -0700

    Added code to read and write the pb struct to/from a file.

--- main.go
+++ main.go
@@ -2,14 +2,60 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"protobuf-to-go/src/simple/simplepb"
+
+	"google.golang.org/protobuf/proto"
 )
 
 func main() {
-	doSimple()
+	sm := doSimple()
+	
+	readAndWriteDemo(sm)
 }
 
-func doSimple() {
+func readAndWriteDemo(sm proto.Message) {
+	writeToFile("simple.bin", sm)
+	sm2 := &simplepb.SimpleMessage{}
+	readFromFile("simple.bin", sm2)
+	fmt.Println(sm2)
+}
+
+func readFromFile(fname string, pb proto.Message) error {
+	in, err := ioutil.ReadFile(fname)
+	if(err != nil) {
+		log.Fatalln("Something went wrong when reading the file", err)
+		return err
+	}
+
+	err = proto.Unmarshal(in, pb)
+	if(err != nil) {
+		log.Fatalln("Couldn't put the bytes into the protocol buffer's struct", err)
+		return err
+	}
+
+	return nil
+}
+
+func writeToFile(fname string, pb proto.Message) error {
+	out, err := proto.Marshal(pb)
+	if err != nil {
+		log.Fatalln("Can't serialize to bytes", err)
+		return err
+	}
+
+	if err := ioutil.WriteFile(fname, out, 0644); err != nil {
+		log.Fatalln("Can't write to file", err)
+		return err
+	}
+
+	fmt.Println("Data has been written")
+
+	return nil
+}
+
+func doSimple() *simplepb.SimpleMessage {
 	sm := simplepb.SimpleMessage{
 		Id:         12345,
 		IsSimple:   true,
@@ -23,4 +69,6 @@ func doSimple() {
 
 	fmt.Println(sm)
 	fmt.Println("The ID is:", sm.GetId())
+
+	return &sm
 }
